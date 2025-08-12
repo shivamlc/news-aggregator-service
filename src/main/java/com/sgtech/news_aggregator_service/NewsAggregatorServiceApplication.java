8fb053ae86bf0bfb7ea989227ee155961db73411
shivamacshack <s.gaur@uq.edu.au>
2025-08-13 05:51:12 +1000

    main branch

---
+++ src/main/java/com/sgtech/news_aggregator_service/NewsAggregatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sgtech.news_aggregator_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsAggregatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsAggregatorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sgtech/news_aggregator_service/NewsAggregatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgtech.news_aggregator_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsAggregatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
